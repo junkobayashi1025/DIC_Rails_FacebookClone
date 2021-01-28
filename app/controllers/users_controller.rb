3691082cd290911e1fafe1f64d95345b11b3eb43
jun kobayashi <junkobayashi1025@gmail.com>
2021-01-28 06:46:48 +0000

    ログイン機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,25 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-  end
+   @user = User.new(user_params)
+   if @user.save
+     session[:user_id] = @user.id
+     redirect_to mypage_path
+   else
+     render :new
+   end
+ end
 
-  def me
-  end
+ def me
+ end
+
+ private
+
+ def user_params
+   params.require(:user).permit(:name, :email, :password,
+                                :password_confirmation)
+ end
 end
